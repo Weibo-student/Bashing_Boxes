50a09471bf66267c562772f4758789d10c597dfc
fatima <fatimazehra7212@gmail.com>
2025-10-15 10:16:00 -0500

    add the remove

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
+list=("Sled" "Milk carton" "Postcard" "Clothes line" "Fire extinguisher" "Handkerchief" "Combat boots" "Mountain" "Bottle opener" "Waffle")
+
 
 while true; do
 	read -p "What do you want to do?" options
 	
 	if [ "$options" = "Print list" ]; then
-
-		list=("Sled" "Milk carton" "Postcard" "Clothes line" "Fire extinguisher" "Handkerchief" "Combat boots" "Mountain" "Bottle opener" "Waffle")
-
 		for str in "${list[@]}"; do
    			echo "$str"
 		done
 	elif [ "$options" = "Print item at X position in list" ]; then
-		read -p "Which one do you want to print" number
+		read -p "Which one do you want to print? " number
+		number=$((number - 1))
 		echo "${list[number]}"
 	elif [ "$options" = "Add item to the list" ]; then
-		read -p "What do you want to add" item
+		read -p "What do you want to add? " item
 		list+=("$item")
+	elif [ "$options" = "Remove item - from X position" ]; then
+		read -p "Which one do you want to remove? " number
+		number=$((number - 1))
+		unset 'list[number]'
 	elif [ "$options" = "Exit" ]; then
 		echo "Bye!"
 		break
